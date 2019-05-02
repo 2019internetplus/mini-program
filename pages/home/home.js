8ffb1504b88d25141c5f251a6db7be2f2005e0e0
victorSeptember <2452508171@qq.com>
2019-05-03 00:00:23 +0800

    lunbo

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,11 @@
-Page({})
+Page({
+  data: {
+    imgUrls:['../../images/lunBo/1.png', '../../images/lunBo/2.png','../../images/lunBo/3.png'],
+    indicatorDots: true,
+    indicatorColor: "white",
+    indicatorSltClr: "#FFA500",
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+  }
+})
