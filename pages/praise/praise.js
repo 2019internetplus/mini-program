2c0c2ffba0b643b4e9604b6f0fb660b8c352c0c2
pray first <unknown@example.com>
2019-05-08 18:47:46 +0800

    夸夸圈ui

--- pages/praise/praise.js
+++ pages/praise/praise.js
@@ -1,1 +1,77 @@
-Page({})
+//index.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    post: [
+      {Message:"123",
+        UpNum:"11",
+        CommentNum:"2",
+        CollectNum:"1"
+      },
+      {Message:"123",
+        UpNum: "10",
+        CommentNum: "3",
+        CollectNum: "4"}
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
