6f2f9f00e133c4ac6ff96dd5c4f433a9c6fb93e0
victorSeptember <2452508171@qq.com>
2019-05-18 15:59:14 +0800

    restart

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,5 @@
+var app = getApp();
+
 Page({
-  data: {
-    imgUrls:['../../images/lunBo/1.png', '../../images/lunBo/2.png','../../images/lunBo/3.png'],
-    indicatorDots: true,
-    indicatorColor: "white",
-    indicatorSltClr: "#FFA500",
-    autoplay: true,
-    interval: 3000,
-    duration: 1000,
-  }
+ 
 })
