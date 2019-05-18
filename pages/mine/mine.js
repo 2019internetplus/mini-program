e8f716c7846a32f17e09740d47b23484c51eb39b
gill_shouyou <799215677>
2019-05-18 18:35:11 +0800

    mine

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -2,11 +2,10 @@ var _app = getApp()
 Page({
   data: { 
     menuitems: [
-      { text: '我的会员', url: '../mymember/mymember', icon: '../../images/会员.png', tips: '' }, 
-      { text: '我的测评', url: '../myassessment/myassessment', icon: '../../images/测评.png', tips: '' }, 
-      { text: '我的收藏', url: '../mycollection/mycollection', icon: '../../images/收藏.png', tips: '' },
-      { text: '我的消息', url: '../mynews/mynews', icon: '../../images/消息.png', tips: '' }, 
-      { text: '我的购买', url: '../mypurchase/mypurchase', icon: '../../images/购买.png', tips: '' }
+      { text: '我的报告', url: '../myreport/myreport', icon: '../../images/我的报告.png' },
+      { text: '我的消息', url: '../mynews/mynews', icon: '../../images/我的消息.png'}, 
+      { text: '使用帮助', url: '../usehelp/usehelp', icon: '../../images/使用帮助.png'},
+      { text: '关于我们', url: '../aboutus/aboutus', icon: '../../images/关于我们.png' }
     ]
   },
   onLoad: function (options) { 
