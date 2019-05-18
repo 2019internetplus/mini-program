a244596a5cff5dd2c7cc0730942e2cbf355541e8
gill_shouyou <799215677>
2019-05-18 18:55:30 +0800

    mine..

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -2,10 +2,10 @@ var _app = getApp()
 Page({
   data: { 
     menuitems: [
-      { text: '我的报告', url: '../myreport/myreport', icon: '../../images/我的报告.png' },
-      { text: '我的消息', url: '../mynews/mynews', icon: '../../images/我的消息.png'}, 
-      { text: '使用帮助', url: '../usehelp/usehelp', icon: '../../images/使用帮助.png'},
-      { text: '关于我们', url: '../aboutus/aboutus', icon: '../../images/关于我们.png' }
+      { text: '我的报告', url: '../myreport/myreport', icon: '../../images/mine/我的报告.png' },
+      { text: '我的消息', url: '../mynews/mynews', icon: '../../images/mine/我的消息.png'}, 
+      { text: '使用帮助', url: '../usehelp/usehelp', icon: '../../images/mine/使用帮助.png'},
+      { text: '关于我们', url: '../aboutus/aboutus', icon: '../../images/mine/关于我们.png' }
     ]
   },
   onLoad: function (options) { 
