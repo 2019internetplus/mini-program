1cee4250108cbbcf20b8af768277f8d7b3bd06c5
gill_shouyou <799215677>
2019-05-25 23:51:13 +0800

    mynotice

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -4,7 +4,7 @@ Page({
   data: { 
     menuitems: [
       { text: '我的报告', url: 'myreport/myreport', icon: '../../images/mine/myreport.png' },
-      { text: '我的消息', url: 'mynews/mynews', icon: '../../images/mine/mynews.png'}, 
+      { text: '我的消息', url: 'mynotice/mynotice', icon: '../../images/mine/mynotice.png'}, 
       { text: '使用帮助', url: 'usehelp/usehelp', icon: '../../images/mine/usehelp.png'},
       { text: '关于我们', url: 'aboutus/aboutus', icon: '../../images/mine/aboutus.png' }
     ]
--- utils/util.js
+++ utils/util.js
@@ -1,35 +1,102 @@
-//数据转化
-function formatNumber(n) {
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
-/**
- * 时间戳转化为年 月 日 时 分 秒
- * number: 传入时间戳
- * format：返回格式，支持自定义，但参数必须与formateArr里保持一致
-*/
-function formatTime(number, format) {
+function obj2uri(obj) {
+  return Object.keys(obj).map(function (k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]);
+  }).join('&');
+}
 
-  var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
-  var returnArr = [];
 
-  var date = new Date(number * 1000);
-  returnArr.push(date.getFullYear());
-  returnArr.push(formatNumber(date.getMonth() + 1));
-  returnArr.push(formatNumber(date.getDate()));
+//字符串转换为时间戳
+function getDateTimeStamp(dateStr) {
+  return Date.parse(dateStr.replace(/-/gi, "/"));
+}
+//格式化时间
+function getDateDiff(dateStr) {
+  var publishTime = getDateTimeStamp(dateStr) / 1000,
+    d_seconds,
+    d_minutes,
+    d_hours,
+    d_days,
+    timeNow = parseInt(new Date().getTime() / 1000),
+    d,
 
-  returnArr.push(formatNumber(date.getHours()));
-  returnArr.push(formatNumber(date.getMinutes()));
-  returnArr.push(formatNumber(date.getSeconds()));
-
-  for (var i in returnArr) {
-    format = format.replace(formateArr[i], returnArr[i]);
+    date = new Date(publishTime * 1000),
+    Y = date.getFullYear(),
+    M = date.getMonth() + 1,
+    D = date.getDate(),
+    H = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds();
+  //小于10的在前面补0
+  if (M < 10) {
+    M = '0' + M;
   }
-  return format;
+  if (D < 10) {
+    D = '0' + D;
+  }
+  if (H < 10) {
+    H = '0' + H;
+  }
+  if (m < 10) {
+    m = '0' + m;
+  }
+  if (s < 10) {
+    s = '0' + s;
+  }
+
+  d = timeNow - publishTime;
+  d_days = parseInt(d / 86400);
+  d_hours = parseInt(d / 3600);
+  d_minutes = parseInt(d / 60);
+  d_seconds = parseInt(d);
+
+  if (d_days > 0 && d_days < 3) {
+    return d_days + '天前';
+  } else if (d_days <= 0 && d_hours > 0) {
+    return d_hours + '小时前';
+  } else if (d_hours <= 0 && d_minutes > 0) {
+    return d_minutes + '分钟前';
+  } else if (d_seconds < 60) {
+    if (d_seconds <= 0) {
+      return '刚刚';
+    } else {
+      return d_seconds + '秒前';
+    }
+  } else if (d_days >= 3 && d_days < 30) {
+    return M + '-' + D + ' ' + H + ':' + m;
+  } else if (d_days >= 30) {
+    return Y + '-' + M + '-' + D + ' ' + H + ':' + m;
+  }
+}
+
+function buttonClicked(self) {
+  self.setData({
+    buttonClicked: true
+  })
+  setTimeout(function () {
+    self.setData({
+      buttonClicked: false
+    })
+  }, 500)
 }
 
 module.exports = {
-  formatNumber: formatNumber,
-  formatTime: formatTime
+  formatTime: formatTime,
+  getDateDiff: getDateDiff,
+  buttonClicked: buttonClicked,
 }
