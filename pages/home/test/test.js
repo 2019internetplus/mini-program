6cad5fd949175944a972caf1f2d64ab01664fd7a
victorSeptember <2452508171@qq.com>
2019-05-30 20:56:54 +0800

    result

--- pages/home/test/test.js
+++ pages/home/test/test.js
@@ -16,89 +16,50 @@ Page({
     type_0: 0,  //自我肯定
     type_1: 0,  //焦虑
     type_2: 0,  //忧郁
-    //title: postData.describe,
+    result: 0,
     choice: {},
     radios: ['A', 'B', 'C','D'],
     list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-    //seconds: 0,
-    //time: '00:00:00',
+    
   },
 
-  randSort: function () {
-    return Math.random() > 0.5 ? 1 : -1;
+  getscore: function(tmp) {
+    if (tmp == 'A') {
+      return 1;
+    } else if (tmp == 'B') {
+      return 2;
+    } else if (tmp == 'C') {
+      return 3;
+    } else if (tmp == 'D') {
+      return 4;
+    }
   },
-
-  setList: function () {
-    var newList = this.data.list.sort(this.randSort);
-    this.setData({
-      list: newList
-    });
-  },
-
-  setRadios: function () {
-    var newRadios = this.data.radios.sort(this.randSort);
-    this.setData({
-      radios: newRadios
-    });
-  },
-
-
-  //点击按钮播放音乐
-  playmusic: function () {
-    const innerAudioContext = wx.createInnerAudioContext()
-    innerAudioContext.autoplay = true
-    innerAudioContext.src = '/data/2310.mp3'
-    innerAudioContext.onPlay(() => {
-      console.log('开始播放')
-    })
-    innerAudioContext.onError((res) => {
-      console.log(res.errMsg)
-      console.log(res.errCode)
-    })
-  },
-
-
 
   select: function (e) {
     var tmp = e.detail.value;
+    console.log(tmp);
     var type = this.data.choice[this.data.index].type;
     if (type == "0") {     //计算“自我肯定”类型题目的得分
       this.setData({
-        type_0: this.data.type_0 + tmp
+        type_0: this.data.type_0 + this.getscore(tmp),
       })
     } else if (type == "1") {    //计算“焦虑”类型题目的得分
       this.setData({
-        type_1: this.data.type_1 + tmp
+        type_1: this.data.type_1 + this.getscore(tmp),
       })
     } else if (type == "2") {     //计算“忧郁”类型题目的得分
       this.setData({
-        type_2: this.data.type_2 + tmp
+        type_2: this.data.type_2 + this.getscore(tmp),
       })
     }
 
    
     this.setData({ 
-      per: this.data.per+10, 
+      per: this.data.per+10,
+      result: this.data.result + this.getscore(tmp)
       })
-    if (tmp == 'A') {
-      this.setData({
-        A: this.data.A + 1
-      })
-    } else if (tmp == 'C') {
-      this.setData({
-        C: this.data.C + 1
-      })
-    } else if (tmp == 'B') {
-      this.setData({
-        B: this.data.B + 1
-      });
-    } else if (tmp == 'D') {
-      this.setData({
-        D: this.data.D + 1
-      })
-    }
     if (this.data.index < 9) {
-      this.setRadios();
+      
       this.setData({
         index: this.data.index + 1,
         clear: false
@@ -110,14 +71,40 @@ Page({
   },
 
   submit: function () {
-    var result = (this.data.A + this.data.B*2 + this.data.C*3 + this.data.D*4)/40*100;    
+        
     //A、B、C、D分别按1，2，3，4分算，再转成100分制
-    console.log(result);
+    console.log(this.data.result);
     console.log(this.data.type_0);
     console.log(this.data.type_1);
     console.log(this.data.type_2);
+    var s1 = Math.round(this.data.result / 40 * 100);
+    var s2 = Math.round(this.data.type_0 / 16 * 100);
+    var s3 = Math.round(this.data.type_1 / 12 * 100);
+    var s4 = Math.round(this.data.type_2 / 12 * 100);
+
+    const commit = wx.getStorageSync('commit_time') == new Date().getDate() ? 1 : 0;
+    
+    const token = wx.getStorageSync('token');
+    const openid = wx.getStorageSync('openid');
+
+    wx.request({
+      url: ' https://api.xumengli.cn/em/v0.1/addTestValue?token=' +  token +'&commit='+ commit +'&openid='+ openid +'&test_value='+ s1 +'&self_affirm='+ s2*0.4 +'&anti_anxiety='+ s3*0.3 +'&anti_melancholy='+ s4*0.3,
+      method: 'PUT',
+      dataType: 'STRING',
+     
+     
+      success: function (res) {
+        wx.setStorageSync('commit_time', new Date().getDate());
+        console.log(res.data.message)
+      },
+      fail: function(error) {
+        console.log(error)
+      }
+
+    })
+
     wx.redirectTo({
-      url: '/pages/home/result/result?result=' + result + '&type_0=' + this.data.type_0/16*100 + '&type_1=' + this.data.type_1/12*100 + '&type_2=' + this.data.type_2/12*100,
+      url: '/pages/home/result/result?result=' + s1 + '&type_0=' + s2 + '&type_1=' + s3 + '&type_2=' + s4
     })
 
     /*
@@ -131,13 +118,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    //timing(this);
-    this.setList();
-    this.setRadios();
     this.setData({
-      A: 0,
-      C: 0,
-      B: 0
+      
     });
 
     var that = this;
